2ba6ae6819c41a60b67af5feb9e7496dddac3063
Humphrey SUI <55611676+humphreysui@users.noreply.github.com>
2022-09-08 13:10:11 +0800

    new location added

--- indexJs.js
+++ indexJs.js
@@ -4,6 +4,15 @@ var venues ={
   '2': ["沈北华强广场", "Living Mall"],
   '3': ["沈阳金拱门食品有限公司道义南大街华强得来速餐厅", "Mcdonald's LivingMall"],
   '4': ["沈阳市沈北新区美滋滋便利店", "Lawson HuaQiang1Qi"],
+  '5': ["沈北万达", "Wanda Shenbei"],
+  '6': ["汇置尚都六期物业服务中心", "Huiland"],
+  '7': ["沈北吾悦广场", "Wu Yue Plaza"],
+  '8': ["沈北新区政务服务中心", "ShenBei Gov Service"],
+  '9': ["#", "#"],
+    
+ 
+
+    
 
 }
 var venueDisplay = document.getElementById("location");
