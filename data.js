2d5826c92a4ffbe265e5ed46e90b154ad802a855
Humphrey SUI <55611676+humphreysui@users.noreply.github.com>
2022-09-09 20:43:18 +0800

    final

---
+++ data.js
@@ -0,0 +1,22 @@
+// getting data
+var venues ={ 
+  // subway stations
+  a: ['二号线师范大学地铁站', 'SNU (SubwayLine2)'],
+  b: ['二号线辽宁大学地铁站', 'Liao Da (SubwayLine2)'],
+  c: ['一号线重工街地铁站', 'Zhong Gong Jie (SubwayLine1)'],
+  //
+  '1': ["华强城二期", "HuaQiangCheng 2 Qi"],
+  '2': ["沈北华强广场", "Living Mall"],
+  '3': ["沈阳金拱门食品有限公司道义南大街华强得来速餐厅", "Mcdonald's LivingMall"],
+  '4': ["沈阳市沈北新区美滋滋便利店", "Lawson HuaQiang1Qi"],
+  '5': ["沈北万达", "Wanda Shenbei"],
+  '6': ["汇置尚都六期物业服务中心", "Huiland"],
+  '7': ["沈北吾悦广场", "Wu Yue Plaza"],
+  '8': ["沈北新区政务服务中心", "ShenBei Gov Service"],
+  '9': ["#", "#"],
+    
+ 
+
+    
+
+}
---
+++ venue.js
@@ -0,0 +1,105 @@
+// get data
+var date = document.getElementById("date");
+var time = document.getElementById("time");
+var vaxTime = document.getElementById("vaxTime");
+// two parts of time
+var ts = document.getElementById("ts");
+var ms = document.getElementById("ms");
+// get element on page
+var venueDisplay = document.getElementById("location");
+var venueName = document.getElementById("venueName");
+var venueList = document.getElementById("venueList");
+
+var btn = document.getElementById("btn");
+
+// populate venue name list
+function showList() {
+
+  for (const key in venues) {
+
+    var opt = `<option value="${venues[key][0]}">${venues[key][1]}</option>`;
+    venueList.innerHTML += opt;
+
+  }
+
+}
+
+venueDisplay.addEventListener('load', showList());
+
+// display date on code page
+const displayDate = () =>{
+  const d = new Date();
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, '0');
+  day = String(d.getDate()).padStart(2, '0');
+
+  return  year + "年" + month + "月" + day + "日";
+}
+
+// display time and sec on code page
+function displayTS() {
+  const d = new Date();
+  const h = d.getHours();
+  const m = d.getMinutes();
+  const s = d.getSeconds();
+  ts.innerHTML = h + "时" + m + "分" + s + "秒";
+   
+}
+
+// display millisec
+function displayMS() {
+  const d = new Date();
+  const mis = d.getMilliseconds();
+  ms.innerHTML = `${mis} <span class='mil'>毫秒</span>`;
+}
+
+// display vax date
+const displayVaxDate = () => {
+  const d = new Date();
+  const year = d.getFullYear();
+  const month = String(d.getMonth() + 1).padStart(2, '0');
+  day = String(d.getDate()).padStart(2, '0');
+
+  return year + "-" + month + "-" + day;
+}
+
+// on click display venue & date time on code
+function displayVenue() {
+  var selectedVenue = document.getElementById("venueList").value;
+  var typedVenue = document.getElementById("vname").value;
+
+ 
+
+  if (selectedVenue != '' && typedVenue == ''){
+    venueName.innerHTML = `<p>${selectedVenue}<p>`;
+    venueDisplay.style.visibility = 'hidden';
+    setInterval(displayTS, 1000);
+    setInterval(displayMS, 1);
+    date.innerHTML = displayDate();
+    vaxTime.innerHTML = displayVaxDate();
+    console.log('selected only');
+  }
+
+  else if (typedVenue != '' && !selectedVenue){
+    venueName.innerHTML = `<p>${typedVenue}<p>`;
+    venueDisplay.style.visibility = 'hidden';
+    setInterval(displayTS, 1000);
+    setInterval(displayMS, 1);
+    date.innerHTML = displayDate();
+    vaxTime.innerHTML = displayVaxDate();
+    console.log('typed only');
+  }
+  else if (!selectedVenue || !typedVenue) {
+    alert("please select a location or type a new one!");
+    console.log('no selceted or typed');
+  } 
+  else{
+    alert("You can only select or type one at a time!")
+  }
+   
+    
+   
+  
+  // TODO: need logic to choose from names
+}
+
